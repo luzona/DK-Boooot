7b83891cb7d89cc429305ae6975b7b33be1f61d2
Luffy12323 <41828478+Luffy12323@users.noreply.github.com>
2019-02-22 12:33:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -635,44 +635,6 @@ if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('
 
 
 
-
-client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!developers.includes(message.author.id)) return;
-      
-  if (message.content.startsWith(adminprefix + 'setplaying')) {
-    client.user.setGame(argresult);
-      message.channel.send(`**Done You Have Been Changed The Playing To ${argresult}**✅ `)
-  } else 
-     if (message.content === (adminprefix + "dkbotleave")) {
-    message.guild.leave();        
-  } else  
-  if (message.content.startsWith(adminprefix + 'setwatching')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`**Done You Have Been Changed The Watching To ${argresult}**✅`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'setlistening')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**Done You Have Been Changed The Listening To ${argresult}**✅`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'setstreaming')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/idk");
-      message.channel.send(`**Done You Have Been Changed The Streaming To ${argresult}**✅`)
-  }
-  if (message.content.startsWith(adminprefix + 'setname')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(`**Changing The Name To ..${argresult}** ✅`)
-} else
-if (message.content.startsWith(adminprefix + 'setavatar')) {
-  client.user.setAvatar(argresult);
-    message.channel.send(`**Changing The Avatar To :${argresult}** ✅`);
-}
-
-
-});
-
-
-
 client.on('message', message => {
     const prefix = '$'
 var args = message.content.split(" ").slice(1);    
