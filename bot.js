ec0df31df6e1422753f9ec4e6e19799037b84f3b
Luffy12323 <41828478+Luffy12323@users.noreply.github.com>
2019-02-24 16:21:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -865,6 +865,67 @@ client.on("message", (message) => {
 });
  
 
+client.on('message', message => {
+    if (message.content.startsWith("-bot")) {
+           let uptime = client.uptime;
+
+    let days = 0;
+    let hours = 0;
+    let minutes = 0;
+    let seconds = 0;
+    let notCompleted = true;
+
+    while (notCompleted) {
+
+        if (uptime >= 8.64e+7) {
+
+            days++;
+            uptime -= 8.64e+7;
+
+        } else if (uptime >= 3.6e+6) {
+
+            hours++;
+            uptime -= 3.6e+6;
+
+        } else if (uptime >= 60000) {
+
+            minutes++;
+            uptime -= 60000;
+
+        } else if (uptime >= 1000) {
+            seconds++;
+            uptime -= 1000;
+
+        }
+
+        if (uptime < 1000)  notCompleted = false;
+
+    }
+let ms = 1000;
+let v1 = new Discord.RichEmbed()
+  v1.setTimestamp(new Date())
+  v1.setColor("RED")
+  v1.setDescription('***__ Collecting Data __***')
+  v1.setFooter("# | DK Bot |") 
+let heroo = new Discord.RichEmbed()
+.setColor('RANDOM')
+.setTimestamp(new Date())
+.setThumbnail(client.user.avatarURL)
+.setAuthor(client.user.username,client.user.avatarURL)
+.addField("MyPrefix :",`**[ ${prefix} ]**`,true)
+.addField("Guilds :","**[ "+client.guilds.size+" ]**",true)
+.addField("Channels :","**[ "+client.channels.size+" ]**",true)
+.addField("Users :","**[ "+client.users.size+" ]**",true)
+.addField("MyName : ","**[ "+client.user.username+" ]**",true)
+.addField("MyID :","**[ "+client.user.id+" ]**",true)
+.addField("RamUsage :",`**[ ${(process.memoryUsage().rss / 1048576).toFixed()}MB ]**`,true)
+.addField("UpTime :",`**[** **Days:** \`${days}\` **Hours:** \`${hours}\` **Minutes:** \`${minutes}\` **Seconds:** \`${seconds}\` **]**`,true)
+.setFooter(" DK . |")
+  message.channel.send({embed:v1}).then(m => m.edit({embed:heroo})),ms; 
+    }
+});
+
+
 client.on('ready', () => {
    console.log(`----------------`);
       console.log(`Desert Bot- Script By : DK Clan`);
