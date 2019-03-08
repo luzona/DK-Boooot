27e2b34ebf8ca9df003ea53e3355db5ca5abf643
Luffy12323 <41828478+Luffy12323@users.noreply.github.com>
2019-03-08 15:33:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -148,7 +148,7 @@ message.channel.awaitMessages(filter, { max: 1, time: 15000, errors: ['time'] })
    const filte = m => m.content.startsWith("لا");
 message.channel.awaitMessages(filte, { max: 1, time: 15000, errors: ['time'] })
 .then(collected =>{ 
-   message.channel.send(`  **${message.author} تم رفض عرضك** `);
+   message.channel.send(`  **${message.author} تم رفض عرضك اعمل نفسك ميت** `);
 })
         
   }
@@ -844,6 +844,83 @@ if(!message.guild.member(client.user).hasPermission("MUTE_MEMBERS")) return mess
   }
  
 });
+
+
+client.on("message", msg=>{
+if(msg.content.startsWith(`${prefix}setRole`)){
+if(!msg.member.hasPermission("ADMINISTRATOR")) return msg.reply("you don't have permission").then(s => {s.delete(1600);})
+msg.reply("منشن الروم الي تبي فيه التفعيل").then(msgs=>{
+  const filter = response => response.author.id === msg.author.id;
+  msg.channel.awaitMessages(filter, { maxMatches: 1, time: 30000, errors: ['time'] })
+  .then( collected =>{
+    msg.delete();
+    let idC = msg.guild.channels.find(c=>c.id == collected.first().mentions.channels.first().id)
+    if(!idC) return msgs.edit("لم اجد الروم");
+     idC = idC.id;
+     msgs.edit(`${msg.author}, ادخل الايموجي الذي تريدة للتفعيل`)
+const filter = response => response.author.id === msg.author.id;
+msg.channel.awaitMessages(filter, { maxMatches: 1, time: 30000, errors: ['time'] })
+.then( collected =>{
+if(!emojiss.hasEmoji(collected.first().mentions._content)) return msgs.edit("ادخل ايموجي صحيح !");
+newemoji = collected.first().mentions._content;
+msg.delete();
+msgs.edit(`${msg.author}, منشن للرتبة الذي تريدها`)
+const filter = response => response.author.id === msg.author.id;
+msg.channel.awaitMessages(filter, { maxMatches: 1, time: 30000, errors: ['time'] })
+.then( collected =>{
+let roleW = collected.first().mentions.roles.first()
+if(!roleW) {
+  let embed = new Discord.RichEmbed()
+  .setColor("#42f4f4")
+  .setTitle(`:x: - منشن الرتبة `);
+  msg.reply(embed).then( z => z.delete(3000)); return
+};
+let role = msg.guild.roles.find(`name`, roleW.name);
+if(!role) {
+  let embed = new Discord.RichEmbed()
+  .setColor("#42f4f4")
+  .setTitle(`:x: - Could't find \`${roleW.name}\` role.`);
+msg.reply(embed).then( msgs => msgs.delete(3000));
+return
+}
+roleNew = role;
+msgs.edit(`${msg.author}, ادخل النص الذي تريدة`)
+const filter = response => response.author.id === msg.author.id;
+msg.channel.awaitMessages(filter, { maxMatches: 1, time: 30000, errors: ['time'] })
+.then( collected =>{
+stringNew = collected.first().mentions._content;
+let channel = msg.guild.channels.get(idC);
+if(!channel) {
+  let embed = new Discord.RichEmbed()
+  .setColor("#42f4f4")
+  .setTitle(`:x: - Could't find \`${idC}\` Channel.`);
+msg.reply(embed).then( msgs => msgs.delete(3000));
+return
+}
+channel.bulkDelete(100)
+channel.send(`@here || @everyone
+${msg.guild.name}© :arrow_down:
+ 
+${stringNew}
+`).then( msgA =>{
+msgA.react(newemoji).then(()=>{
+  const Ac = (reaction, user) => reaction.emoji.name === newemoji && !user.bot;
+  const Acc = msgA.createReactionCollector(Ac, {time: 120000});
+  Acc.on("collect", r=>{
+  let member = msg.guild.members.get(r.users.last().id);
+  if(!member) return;
+  r.remove(member.user);
+if(member.roles.find(r=>r.name == roleNew.name)) return;
+    member.addRole(roleNew);
+  channel.send(`${member.user}, تم تفعيلك`).then(z => z.delete(1500));
+})})})
+}).catch(e => {console.log(e.message)});  
+}).catch(e => {console.log(e.message)});
+}).catch(e => {console.log(e.message)});
+}).catch(e => {console.log(e.message)});
+})
+///
+}});
 
 
   client.on('message', message => {
